d58d9ac263c5bfa53c72d7f9866010a17030081a
Nate Stemo <natestemo@hotmail.com>
2024-03-19 14:29:19 -0500

    successfully scrapes meets

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,124 @@
 const puppeteer = require('puppeteer')
 let fs = require('fs');
 
-let sitemaps = [
-    {
-        path: './sitemap_data/athlete/',
-        url:'https://liftoracle.com/sitemap-athletes-1.xml',
-    },
-    {
-        path: './sitemap_data/athlete/',
-        url:'https://liftoracle.com/sitemap-athletes-2.xml',
-    },
-    {
-        path: './sitemap_data/static/',
-        url:'https://liftoracle.com/sitemap-static.xml',
-    },
-    {
-        path: './sitemap_data/meet/',
-        url:'https://liftoracle.com/sitemap-meets-1.xml',
-    },
-]
+//https://liftoracle.com/sitemap.xml 
+// let sitemaps = [
+//     {
+//         path: './sitemap_data/athlete/',
+//         url:'https://liftoracle.com/sitemap-athletes-1.xml',
+//     },
+//     {
+//         path: './sitemap_data/athlete/',
+//         url:'https://liftoracle.com/sitemap-athletes-2.xml',
+//     },
+//     {
+//         path: './sitemap_data/static/',
+//         url:'https://liftoracle.com/sitemap-static.xml',
+//     },
+//     {
+//         path: './sitemap_data/meet/',
+//         url:'https://liftoracle.com/sitemap-meets-1.xml',
+//     },
+//  ]
 
 
-async function scrapeStatic(){
-    let data = {
-        path: './sitemap_data/static/',
-        url:'https://liftoracle.com/sitemap-static.xml',
-    }
 
-    console.log('scraping static page')
+
+//utilities
+
+function writeHtml(htmlData:string, fileName:string){
+    const stream = fs.createWriteStream(fileName);
+    stream.once('open', ()=>{
+        stream.end(htmlData);
+    })
 }
+
+
+
+
 
 async function scrapeMeets(){
-    let data = {
-        path: './sitemap_data/meet/',
-        url:'https://liftoracle.com/sitemap-meets-1.xml',
-    }
+    // let path = './sitemap_data/meet/';
+    let url ='https://liftoracle.com/sitemap-meets-1.xml';
 
     console.log('scraping meets')
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({width:1500, height:1000})
+    await page.goto(url, {
+    waitUntil: 'networkidle0'
+    })
+
+    await page.screenshot({fullPage: false, path:'meetpage.png'})
+
+    let siteMapMeets = await page.evaluate(()=>{
+    let selector = "url loc"
+    let elArr: any = Array.from(document.querySelectorAll(`${selector}`))
+    elArr = elArr.map((x:any)=>{
+        let el = x.textContent
+        return el;
+    })
+    return elArr
+    })
+    
+    console.log(`found ${siteMapMeets.length} meets in the sitemap`)
+    await browser.close()
+
+    for(let i = 0; i < 4; i++){
+    // for(let i = 0; i < siteMapMeets.length; i++){
+        await crawlMeetPage(siteMapMeets[i])
+    }
+    console.log('crawled all meet pages');
+
 }
 
-async function scrapeAthletes(){
-    let data = {
-        path: './sitemap_data/athlete/',
-        urls:[
-            'https://liftoracle.com/sitemap-athletes-1.xml',
-            'https://liftoracle.com/sitemap-athletes-2.xml',
-        ]
+async function crawlMeetPage(url:string){
+    let name = url.split('meet/')[1].replace(/['"]/g, '')
+    let formattedName = name.replace(/\s+/g, '_')
+    let fileName = './sitemap_data/meet/' + formattedName + '.html';
+
+    try{
+        console.log('crawling meet page: ' + url)
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.setViewport({width:1500, height:1000})
+        await page.goto(url, {
+            waitUntil: 'networkidle0'
+        })
+               
+        const htmlContent = await page.evaluate(()=>{
+            return document.documentElement.outerHTML;
+        })
+    
+        writeHtml(htmlContent, fileName)
+        await browser.close();
+        
+        //if log file exists
+        fs.appendFileSync('./sitemap_data/meet/log/success.csv', fileName + '|' + url + '\n');
+        //else make the log file
+        //same thing for the catch
+
+    }catch(e){
+        fs.appendFileSync('./sitemap_data/meet/log/error.csv', fileName + '|' + url + '\n');
     }
+}
+
+
+async function scrapeAthletes(){
+    let urls = [
+            'https://liftoracle.com/sitemap-athletes-1.xml',
+            'https://liftoracle.com/sitemap-athletes-2.xml'
+        ];
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setViewport({width:1500, height:1000})
-    await page.goto(data.urls[0], {
+    await page.goto(urls[0], {
         waitUntil: 'networkidle0'
     })
     const page2 = await browser.newPage()
     await page2.setViewport({width:1500, height:1000})
-    await page2.goto(data.urls[1], {
+    await page2.goto(urls[1], {
         waitUntil: 'networkidle0'
     })
     
@@ -92,16 +154,49 @@ async function scrapeAthletes(){
         await crawlAthletePage(allSitemapAthletes[i])
     }
     console.log('crawled all athlete pages');
-    
+
 }
 
 
+async function scrapeStatic(){
+    const url = 'https://liftoracle.com/sitemap-static.xml';
+    console.log('scraping static page')
 
-//https://liftoracle.com/sitemap.xml 
-async function crawlAthletePage(url:string){
-    let name = url.split('athlete/')[1].replace(/['"]/g, '')
-    let formattedName = name.replace(/\s+/g, '_')
-    let fileName = './sitemap_data/athlete/' + formattedName + '.html';
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({width:1500, height:1000})
+    await page.goto(url, {
+        waitUntil: 'networkidle0'
+    })
+    // console.log('got here')
+    await page.screenshot({fullPage: false, path:'static.png'})
+    let staticPages = await page.evaluate(()=>{
+        let selector = "url loc"
+        let elArr: any = Array.from(document.querySelectorAll(`${selector}`))
+        elArr = elArr.map((x:any)=>{
+            let el = x.textContent
+            return el;
+        })
+        return elArr
+    })
+
+    await browser.close();
+
+    //loop thru static pages and crawl each page
+    for(let i = 0; i< staticPages.length; i++){
+        await crawlStaticPage(staticPages[i]);
+    }
+
+    console.log('crawled all static pages')
+
+}
+
+async function crawlStaticPage(url:string){
+    console.log(url)
+    let name = url.split('.com/')[1].replace(/['"]/g, '')
+    if(name.length == 0) name = 'home';
+
+    let fileName = './sitemap_data/static/' + name + '.html';
 
     function writeHtml(htmlData:string, fileName:string){
         const stream = fs.createWriteStream(fileName);
@@ -111,7 +206,7 @@ async function crawlAthletePage(url:string){
     }
 
     try{
-        console.log('crawling specific athlete')
+        console.log('crawling static page: ' + url)
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.setViewport({width:1500, height:1000})
@@ -128,10 +223,42 @@ async function crawlAthletePage(url:string){
         writeHtml(htmlContent, fileName)
         await browser.close();
         
-        fs.appendFileSync('./sitemap_data/athlete/success.csv', fileName + '|' + url + '\n');
+        //if log file exists
+        fs.appendFileSync('./sitemap_data/static/log/success.csv', fileName + '|' + url + '\n');
+        //else make the log file
+
+    }catch(e){
+        fs.appendFileSync('./sitemap_data/static/log/error.csv', fileName + '|' + url + '\n');
+    }
+}
+
+
+async function crawlAthletePage(url:string){
+    let name = url.split('athlete/')[1].replace(/['"]/g, '')
+    let formattedName = name.replace(/\s+/g, '_')
+    let fileName = './sitemap_data/athlete/' + formattedName + '.html';
+
+
+    try{
+        console.log('crawling athlete: ' + url)
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.setViewport({width:1500, height:1000})
+        await page.goto(url, {
+            waitUntil: 'networkidle0'
+        })
+           
+        const htmlContent = await page.evaluate(()=>{
+            return document.documentElement.outerHTML;
+        })
+    
+        writeHtml(htmlContent, fileName)
+        await browser.close();
+        
+        fs.appendFileSync('./sitemap_data/athlete/log/success.csv', fileName + '|' + url + '\n');
         
     }catch(e){
-        fs.appendFileSync('./sitemap_data/athlete/error.csv', fileName + '|' + url + '\n');
+        fs.appendFileSync('./sitemap_data/athlete/log/error.csv', fileName + '|' + url + '\n');
     }
 }
 
@@ -165,25 +292,25 @@ async function getSitemapPages():Promise<string[]>{
 async function run(): Promise<void>{
     console.log('running sitmap crawler');
 
-    await scrapeAthletes();
+    // await scrapeAthletes();
     await scrapeMeets();
-    await scrapeStatic();
+    // await scrapeStatic();
     
     
 }
 
 
-function writeHtml(htmlData:string, url:string){
-    let name = url.split('athlete/')[1].replace(/['"]/g, '')
-    let formattedName = name.replace(/\s+/g, '_')
-    let fileName = './sitemap_data/athlete/' + formattedName + '.html';
+// function writeHtml(htmlData:string, url:string){
+//     let name = url.split('athlete/')[1].replace(/['"]/g, '')
+//     let formattedName = name.replace(/\s+/g, '_')
+//     let fileName = './sitemap_data/athlete/' + formattedName + '.html';
 
-    const stream = fs.createWriteStream(fileName);
+//     const stream = fs.createWriteStream(fileName);
     
-    stream.once('open', ()=>{
-        stream.end(htmlData);
-    })
-}
+//     stream.once('open', ()=>{
+//         stream.end(htmlData);
+//     })
+// }
 
 
 
